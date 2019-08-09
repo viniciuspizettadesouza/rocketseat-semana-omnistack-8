c26a440869f842678d6eabd78b0d330b664b765e
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-08-09 10:58:20 -0300

    Layout da login page concluído

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Login.css';
+
+import logo from '../assets/logo.svg';
+
+export default function Login() {
+    return (
+        <div className="login-container">
+            <form>
+                <img src={logo} alt="Tindev" />
+                <input placeholder="Digite seu usuário no Github" />
+                <button type="submit">Enviar</button>
+
+            </form>
+        </div>
+    );
+}
