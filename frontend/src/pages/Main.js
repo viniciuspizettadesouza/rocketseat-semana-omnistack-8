059e10d8458610b7f06e9a4f320cd657d933bec9
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-08-12 13:03:46 -0300

    finalizado front end
    funcionalidades adicionadas:
    botões de like e deslike ok;
    ao pressionar um botao, o card é apagado da tela;
    quando acabarem os cards uma messagem aparece na tela avisando que acabaram;
    ao clicar no logo do tindev da pagina /dev o user é direcionado de volta p pagina inicial;

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Main.css';
 
 import api from '../services/api';
@@ -22,30 +23,50 @@ export default function Main({ match }) {
         loadUsers();
     }, [match.params.id])
 
+    async function handleLike(id) {
+        await api.post(`/devs/${id}/likes`, null, {
+            headers: { user: match.params.id },
+        })
+        setUsers(users.filter(user => user._id !== id));
+    }
+    async function handleDislike(id) {
+        await api.post(`/devs/${id}/dislikes`, null, {
+            headers: { user: match.params.id },
+        })
+        setUsers(users.filter(user => user._id !== id));
+    }
+
     return (
         <div className="main-container">
-            <img src={logo} alt="Tindev" />
-            <ul>
-                {users.map(user => (
-                    <li key={user._id}>
-                        <img src={user.avatar} alt="Avatar" />
-                        <footer>
-                            <strong>{user.name}</strong>
-                            <p>{user.bio}</p>
-                        </footer>
-                        <div className="buttons">
-                            <button type="button">
-                                <img src={dislike} alt="Dislike" />
-                            </button>
-                            <button>
-                                <img src={like} alt="Like" />
-                            </button>
-                        </div>
-                    </li>
+            <Link to="/">
+                <img src={logo} alt="Tindev" />
+            </Link>
+            {users.length > 0 ? (
 
-                ))}
+                <ul>
+                    {users.map(user => (
+                        <li key={user._id}>
+                            <img src={user.avatar} alt="Avatar" />
+                            <footer>
+                                <strong>{user.name}</strong>
+                                <p>{user.bio}</p>
+                            </footer>
+                            <div className="buttons">
+                                <button type="button" onClick={() => handleDislike(user._id)}>
+                                    <img src={dislike} alt="Dislike" />
+                                </button>
+                                <button type="button" onClick={() => handleLike(user._id)}>
+                                    <img src={like} alt="Like" />
+                                </button>
+                            </div>
+                        </li>
 
-            </ul>
+                    ))}
+                </ul>
+            ) : (
+                    <div className="empty">Acabou :(</div>
+                )}
+
         </div>
     );
 }
